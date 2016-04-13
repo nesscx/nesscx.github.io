939775802ebd7d1f7a3dba0f3815f19d97e87050
Jorge Andrés Pérez Montás <nesscx@gmail.com>
2016-04-13 16:43:18 -0400

    Last Commit

---
+++ Documents/Cinema/New folder/WebCine-develop/Web/CinemaWeb-SomeViews/Cinema/Cinema/Controllers/HomeController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Cinema.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Inicio()
+        {
+            return View();
+        }
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult Location()
+        {
+            return View();
+        }
+        public ActionResult MovieList()
+        {
+            return View();
+        }
+        public ActionResult SessionManagement()
+        {
+            return View();
+        }
+        public ActionResult SessionManagementModifie()
+        {
+            return View();
+        }
+        public ActionResult SessionManagementCreate()
+        {
+            return View();
+        }
+        public ActionResult RoomManagement()
+        {
+            return View();
+        }
+        public ActionResult RoomManagementModifieMode()
+        {
+            return View();
+        }
+        public ActionResult RoomManagementViewMode()
+        {
+            return View();
+        }
+        public ActionResult CinemaManagement()
+        {
+            return View();
+        }
+        public ActionResult CinemaManagementViewMode()
+        {
+            return View();
+        }
+        public ActionResult CinemaManagementModifieMode()
+        {
+            return View();
+        }
+        public ActionResult TarjetaPage()
+        {
+            return View();
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Documents/Cinema/New folder/WebCine-develop/Web/CinemaWeb-SomeViews/Cinema/Cinema/Controllers/MoviesController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Cinema.Models;
+
+namespace Cinema.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private CinemaTickResEntities db = new CinemaTickResEntities();
+        
+        // GET: Movies
+        public ActionResult Index()
+        {
+            var movies = db.Movies.Include(m => m.Film_Classification);
+            return View(movies.ToList());
+        }
+
+        // GET: Movies/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Movie movie = db.Movies.Find(id);
+            if (movie == null)
+            {
+                return HttpNotFound();
+            }
+            return View(movie);
+        }
+
+        // GET: Movies/Create
+        public ActionResult Create()
+        {
+            ViewBag.Film_Classification_ID = new SelectList(db.Film_Classification, "Film_Classification_ID", "Classification_Description");
+            return View();
+        }
+
+        // POST: Movies/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Movie_ID,Title,Sypnosis,Release_Date,Image_URL,Film_Classification_ID,duration")] Movie movie)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Movies.Add(movie);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Film_Classification_ID = new SelectList(db.Film_Classification, "Film_Classification_ID", "Classification_Description", movie.Film_Classification_ID);
+            return View(movie);
+        }
+
+        // GET: Movies/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Movie movie = db.Movies.Find(id);
+            if (movie == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Film_Classification_ID = new SelectList(db.Film_Classification, "Film_Classification_ID", "Classification_Description", movie.Film_Classification_ID);
+            return View(movie);
+        }
+
+        // POST: Movies/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Movie_ID,Title,Sypnosis,Release_Date,Image_URL,Film_Classification_ID,duration")] Movie movie)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(movie).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Film_Classification_ID = new SelectList(db.Film_Classification, "Film_Classification_ID", "Classification_Description", movie.Film_Classification_ID);
+            return View(movie);
+        }
+
+        // GET: Movies/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Movie movie = db.Movies.Find(id);
+            if (movie == null)
+            {
+                return HttpNotFound();
+            }
+            return View(movie);
+        }
+
+        // POST: Movies/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Movie movie = db.Movies.Find(id);
+            db.Movies.Remove(movie);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
